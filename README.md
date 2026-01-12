0658b7589abbc9626dcc7c62fbfda4c47c0f228e
Lorenzo <lorenzofrabi@gmail.com>
2026-01-12 02:05:57 +0000

    adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clon-tabnews
+
 Projeto para aprender pprogramação
--- pages/pg1.js
+++ pages/pg1.js
@@ -1,5 +1,5 @@
-function Home () { 
-    return <h1>Quanto tempo não é mesmo?</h1>
+function Home() {
+  return <h1>Quanto tempo não é mesmo?</h1>;
 }
 
 export default Home;
